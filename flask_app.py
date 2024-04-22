025b8748d0b6cc54aff639e2cc5e27902f76315a
Daisy Li <daisy.li14308@gmail.com>
2024-04-21 20:15:09 -0400

    wheeeeee

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template("main.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
